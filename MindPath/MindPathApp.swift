aede83c87edb6bfc9398acf0b9c05a4e58760713
Manon Le Bihan <manonlebihan1999@gmail.com>
2025-07-03 13:46:41 -0400

    Initial Commit

---
+++ MindPath/MindPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindPathApp.swift
+//  MindPath
+//
+//  Created by Manon Le Bihan on 03/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MindPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
